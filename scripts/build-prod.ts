a549407369ad0a241648fb11219a7b99f91521cc
Alex Price <alexdennisprice@gmail.com>
2024-07-24 07:55:02 +0100

    fix: fixed prod static cp for render

--- scripts/build-prod.ts
+++ scripts/build-prod.ts
@@ -4,6 +4,6 @@ await Promise.all([
   $`bun install`,
   $`bunx tailwindcss -i ./src/styles/root.css -o ./static/root.css`,
   $`bun build ./src/scripts/index.js --outdir ./static`,
-  $`cp ./src/scripts/deps/htmx.min.js ./static`,
-  $`cp ./src/scripts/deps/alpine.min.js ./static`,
+  $`cp ./src/scripts/deps/htmx.min.js ./static/htmx.min.js`,
+  $`cp ./src/scripts/deps/alpine.min.js ./static/alpine.min.js`,
 ]);
--- scripts/dev.ts
+++ scripts/dev.ts
@@ -3,7 +3,7 @@ import { $ } from "bun";
 await Promise.all([
   $`bunx tailwindcss -i ./src/styles/root.css -o ./static/root.css --watch`,
   $`bun build ./src/scripts/index.js --outdir ./static --watch`,
-  $`cp ./src/scripts/deps/htmx.min.js ./static`,
-  $`cp ./src/scripts/deps/alpine.min.js ./static`,
+  $`cp ./src/scripts/deps/htmx.min.js ./static/htmx.min.js`,
+  $`cp ./src/scripts/deps/alpine.min.js ./static/alpine.min.js`,
   $`bun run --hot src/index.tsx`,
 ]);
